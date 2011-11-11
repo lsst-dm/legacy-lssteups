4a3fe7dcc957adaae724ec371edc5ba7a11941f2
jbosch <jbosch@git.lsstcorp.org>
2011-11-11 19:45:57 +0000

    removed declare from 'scons install' lines to avoid EUPS locking problems

--- lib/build_functions.sh
+++ lib/build_functions.sh
@@ -210,7 +210,7 @@ function simplemake {
 }
 
 #@
-#  run "scons install declare".  The $sconsopt variable, set to "opt=3" by 
+#  run "scons install".  The $sconsopt variable, set to "opt=3" by 
 #  default, will be included in the scons command line.
 #
 function simplescons {
@@ -224,9 +224,9 @@ function simplescons {
     ver=
     [ -n "$version" ] && ver="version=$version"
 
-    echo scons $sconsopt $idir $ver install declare $*
-    echo scons $sconsopt $idir $ver install declare $* >> $buildlog 
-    scons $sconsopt $idir $ver install declare $* >> $buildlog 2>&1 || {
+    echo scons $sconsopt $idir $ver install $*
+    echo scons $sconsopt $idir $ver install $* >> $buildlog 
+    scons $sconsopt $idir $ver install $* >> $buildlog 2>&1 || {
         echo "scons ..."
         tail -20 $buildlog
         echo "$prog: scons install failed; see $PWD/$buildlog for details"
@@ -360,12 +360,6 @@ function unpack_tar_and_build {
     fi
     unpacking_and_building=
 
-    # the specific build operation above may have declared the product to
-    # EUPS; however, if it has not, we would rather have the eups distrib 
-    # wrapper handle this
-    #
-    # ensure_declare
-
     return 0
 }
 
@@ -419,42 +413,3 @@ function selfsetup {
         fi
     fi
 }
-
-#@
-#  ensure that the product is declared to EUPS.  
-#  
-function ensure_declare {
-
-    if [ ! -d "$installdir" ]; then
-        echo $prog: $product $version does not appear to be installed, yet
-        return 1
-    fi
-
-    mkdir -p "$installdir/ups"
-    if [ ! -e "$installdir/ups/$product.table" ]; then
-
-        # find a table file to install
-        local tfile="ups/$product.table"
-        [ -f "$tfile" ] || tfile="$builddir/$product.table"
-
-        if [ ! -f "$tfile" ]; then
-            # try to get one from the server
-            local path=$serverpath
-            [ -n "$path" ] || path=$product/$version
-            tfile=`fetch $path/$product.table` || return 1
-        fi
-
-        # install it
-        cp $tfile "$installdir/ups"
-    fi
-    
-    if [ ! -f "$installdir/ups/$product.table" ]; then 
-        echo $prog: Failed to install table file into "$installdir/ups"
-        return 1
-    fi
-
-    eups expandtable -i "$installdir/ups/$product.table" && \
-        eups declare -r "$installdir" 
-    return $?
-}
-
